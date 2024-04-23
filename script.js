f9905e1e10f37088f0cbcce65cc51e2849a9af89
Robin62066 <rsrobinsinghrajput@gmail.com>
2024-04-23 14:34:21 +0530

    simple to see waither of your city

---
+++ script.js
@@ -0,0 +1,13 @@
+const apikey ="d83e6e8580413ecf18e17443dd07d39a";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const seachBox = document.querySelector(".search input");
+const seachBtn = document.querySelector(".search button");
+const weatherIcon =document.querySelector(".weather-icon");
+
+
+
+
+
+
+
+
